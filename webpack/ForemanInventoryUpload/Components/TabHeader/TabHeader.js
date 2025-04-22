5e247719f1499cd7fec77f88250012d575481491
Lucy Fu <lufu@redhat.com>
2025-04-22 14:56:24 -0400

    SAT-31456 - Label button as Generate report for disconnected

--- webpack/ForemanInventoryUpload/Components/TabHeader/TabHeader.js
+++ webpack/ForemanInventoryUpload/Components/TabHeader/TabHeader.js
@@ -1,40 +1,51 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
 import PropTypes from 'prop-types';
 import { Grid, Button, Icon } from 'patternfly-react';
 import { noop } from 'foremanReact/common/helpers';
 import { sprintf, translate as __ } from 'foremanReact/common/I18n';
+import { selectSubscriptionConnectionEnabled } from '../InventorySettings/InventorySettingsSelectors';
 import { isExitCodeLoading } from '../../ForemanInventoryHelpers';
 import './tabHeader.scss';
 
-const TabHeader = ({ exitCode, onRestart, onDownload, toggleFullScreen }) => (
-  <Grid.Row className="tab-header">
-    <Grid.Col sm={6}>
-      <p>{sprintf(__('Exit Code: %s'), exitCode)}</p>
-    </Grid.Col>
-    <Grid.Col sm={6}>
-      <div className="tab-action-buttons">
-        {onRestart ? (
-          <Button
-            bsStyle="primary"
-            onClick={onRestart}
-            disabled={isExitCodeLoading(exitCode)}
-          >
-            {__('Generate and upload report')}
+const TabHeader = ({ exitCode, onRestart, onDownload, toggleFullScreen }) => {
+  const subscriptionConnectionEnabled = useSelector(
+    selectSubscriptionConnectionEnabled
+  );
+  const buttonGenerateLabel = subscriptionConnectionEnabled
+    ? __('Generate and upload report')
+    : __('Generate report');
+
+  return (
+    <Grid.Row className="tab-header">
+      <Grid.Col sm={6}>
+        <p>{sprintf(__('Exit Code: %s'), exitCode)}</p>
+      </Grid.Col>
+      <Grid.Col sm={6}>
+        <div className="tab-action-buttons">
+          {onRestart ? (
+            <Button
+              bsStyle="primary"
+              onClick={onRestart}
+              disabled={isExitCodeLoading(exitCode)}
+            >
+              {buttonGenerateLabel}
+            </Button>
+          ) : null}
+          {onDownload ? (
+            <Button onClick={onDownload}>
+              {__('Download Report')} <Icon name="download" />
+            </Button>
+          ) : null}
+          <Button onClick={toggleFullScreen}>
+            {__('Full Screen')}
+            <Icon name="arrows-alt" />
           </Button>
-        ) : null}
-        {onDownload ? (
-          <Button onClick={onDownload}>
-            {__('Download Report')} <Icon name="download" />
-          </Button>
-        ) : null}
-        <Button onClick={toggleFullScreen}>
-          {__('Full Screen')}
-          <Icon name="arrows-alt" />
-        </Button>
-      </div>
-    </Grid.Col>
-  </Grid.Row>
-);
+        </div>
+      </Grid.Col>
+    </Grid.Row>
+  );
+};
 
 TabHeader.propTypes = {
   onRestart: PropTypes.func,
--- webpack/ForemanInventoryUpload/Components/TabHeader/__tests__/TabHeader.test.js
+++ webpack/ForemanInventoryUpload/Components/TabHeader/__tests__/TabHeader.test.js
@@ -1,13 +1,51 @@
-import { testComponentSnapshotsWithFixtures } from '@theforeman/test';
-
+import React from 'react';
+import { Provider } from 'react-redux';
+import configureMockStore from 'redux-mock-store';
+import thunk from 'redux-thunk';
+import { screen, render } from '@testing-library/react';
+import { noop } from 'foremanReact/common/helpers';
 import TabHeader from '../TabHeader';
 
-const fixtures = {
-  'render without Props': {},
-  /** fixtures, props for the component */
-};
+const middlewares = [thunk];
+const mockStore = configureMockStore(middlewares);
 
 describe('TabHeader', () => {
-  describe('rendering', () =>
-    testComponentSnapshotsWithFixtures(TabHeader, fixtures));
+  test('when subscription connection is enabled', () => {
+    const renderOptions = {
+      API: {
+        INVENTORY_SETTINGS: {
+          response: { subscriptionConnectionEnabled: true },
+        },
+      },
+    };
+    const store = mockStore(renderOptions);
+
+    render(
+      <Provider store={store}>
+        <TabHeader exitCode="exit 0" onRestart={noop} toggleFullScreen={noop} />
+      </Provider>
+    );
+    expect(screen.queryAllByText('Generate and upload report')).toHaveLength(1);
+    expect(screen.queryAllByText('Full Screen')).toHaveLength(1);
+  });
+
+  test('when subscription connection is not enabled', () => {
+    const renderOptions = {
+      API: {
+        INVENTORY_SETTINGS: {
+          response: { subscriptionConnectionEnabled: false },
+        },
+      },
+    };
+    const store = mockStore(renderOptions);
+
+    render(
+      <Provider store={store}>
+        <TabHeader exitCode="exit 0" onRestart={noop} toggleFullScreen={noop} />
+      </Provider>
+    );
+
+    expect(screen.queryAllByText('Generate report')).toHaveLength(1);
+    expect(screen.queryAllByText('Full Screen')).toHaveLength(1);
+  });
 });
